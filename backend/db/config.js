d391e41f76dc540d894d7d3ec782e5a07cce236e
bapanman <bapmon78@gmail.com>
2023-09-06 21:47:39 +0530

    DB STRING UPDATE

--- backend/db/config.js
+++ backend/db/config.js
@@ -2,7 +2,7 @@ const mongoose= require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const mongodb_url = process.env.MONGODB_URI || `\mongodb+srv://${process.env.DB_USERNANE}:${process.env.DB_PASSWORD}@ecom-cluster.bkubgim.mongodb.net/?retryWrites=true&w=majority`
+const mongodb_url = process.env.MONGODB_URI || `mongodb+srv://${process.env.DB_USERNANE}:${process.env.DB_PASSWORD}@ecom-cluster.bkubgim.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(mongodb_url)
 
